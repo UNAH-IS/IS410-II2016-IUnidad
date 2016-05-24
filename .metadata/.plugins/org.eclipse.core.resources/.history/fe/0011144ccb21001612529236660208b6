42edd1c51c99a1d8b003ecae4f9225788df48e44
Erick Reyes <erick.marin.v@gmail.com>
2016-05-24 11:00:44 -0600

    Conceptos basicos, clases

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/fe/0011144ccb21001612529236660208b6
@@ -0,0 +1,15 @@
+package implementacion;
+
+import clases.Producto;
+
+public class Principal {
+	//Declaracion de variables globales en la clase
+	public static void main(String[] args) {
+		//Declaracion de variables locales
+		Producto p = new Producto(); //Objeto o instancia p, Instanciacion
+		
+		p.setNombreProducto("Desodorante OldSpice");
+		System.out.println(p.getNombreProducto());
+	}
+
+}
--- EjemploClase/src/clases/Producto.java
+++ EjemploClase/src/clases/Producto.java
@@ -1,12 +1,39 @@
 package clases;
 
+//Plain Old Java Object/Java Bean:
+//Atributos privados
+//Metodos sets  y gets
+//Constructor con todos los atributos como parametros
+//Constructor vacio
+//Metodo toString
 public class Producto {
 	private int codigoProducto;
 	private String nombreProducto;
 	private String marca;
 	private float precio;
 	private float impuesto;
-	
+	public static int valor_estatico;
+
+	public Producto(
+			int codigoProducto,
+			String nombreProducto,
+			String marca,
+			float precio,
+			float impuesto
+	) {
+		this.codigoProducto = codigoProducto;
+		this.nombreProducto = nombreProducto;
+		this.marca = marca;
+		this.precio = precio;
+		this.impuesto = impuesto;
+	}
+
+	public Producto(String nombreProducto){
+		this.nombreProducto = nombreProducto;
+	}
+
+	public Producto(){}
+
 	public int getCodigoProducto() {
 		return codigoProducto;
 	}
@@ -36,5 +63,37 @@ public class Producto {
 	}
 	public void setImpuesto(float impuesto) {
 		this.impuesto = impuesto;
-	}		
+	}
+
+	public void mostrarInformacion(){
+		System.out.println(nombreProducto+ "("+marca+")");
+	}
+
+	public void mostrarInformacion(String marca){
+		this.marca = marca;
+		mostrarInformacion();
+	}
+
+	public void mostrarInformacion(int precio){
+		this.precio = precio;
+		System.out.println("Se cambio el precio a: "+this.precio);
+		mostrarInformacion();
+	}
+
+	@Override
+	public String toString() {
+		return "Producto [codigoProducto=" + codigoProducto + ", nombreProducto=" + nombreProducto + ", marca=" + marca
+				+ ", precio=" + precio + ", impuesto=" + impuesto + "]";
+	}
+
+	//El metodo toString usualmente se utiliza para mostrar un objeto como Texto
+	/*public String toString(){
+		return 	codigoProducto + "\t" +
+				nombreProducto +"\t" +
+				marca +"\t" +
+				precio +"\t" +
+				impuesto +"\t";
+	}*/
+
+
 }
