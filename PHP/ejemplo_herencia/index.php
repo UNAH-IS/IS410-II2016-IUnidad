9effbbfab63759b5d0a152c5f08a5e802637f784
Erick Reyes <erick.marin.v@gmail.com>
2016-08-10 21:24:14 -0600

    Actualizacion

--- PHP/ejemplo_herencia/index.php
+++ PHP/ejemplo_herencia/index.php
@@ -1,6 +1,37 @@
 <?php
 	include_once("class/class_alumno.php");
 	include_once("class/class_maestro.php");
+
+	function mostrarInformacion($objeto){
+		echo $objeto->toString() . "<br>";
+	}
+
+	function mostrarMaestros($arreglo){
+		echo "<h1>Listado de Maestros</h1>";
+		for($i=0;$i<count($arreglo);$i++){
+			//$objeto instanceof Clase
+			if ($arreglo[$i] instanceof Maestro)
+				mostrarInformacion($arreglo[$i]);
+		}
+	}
+
+	function mostrarAlumnos($arreglo){
+		echo "<h1>Listado de Alumnos</h1>";
+		for($i=0;$i<count($arreglo);$i++){
+			//$objeto instanceof Clase
+			if ($arreglo[$i] instanceof Alumno)
+				mostrarInformacion($arreglo[$i]);
+		}
+	}
+
+	function mostrarPersonas($arreglo){
+		echo "<h1>Listado de personas que no son maestros ni alumnos</h1>";
+		for($i=0;$i<count($arreglo);$i++){
+			//$objeto instanceof Clase
+			if (!$arreglo[$i] instanceof Alumno && !$arreglo[$i] instanceof Maestro)
+				mostrarInformacion($arreglo[$i]);
+		}
+	}
 ?>
 <!DOCTYPE html>
 <html>
@@ -13,11 +44,39 @@
 			//$nombre, $apellido, $genero,
 			//$edad, $carrera, $horario
 			$p = new Persona("Juan","Perez","M",45,"Ing. Sistemas","Matutino");
+			$p1 = new Persona("Luis","Perez","M",45,"Ing. Sistemas","Matutino");
+			$p2 = new Persona("Victor","Perez","M",45,"Ing. Sistemas","Matutino");
+
 			$a = new Alumno("Pedro","Rodriguez","M",50,"Ing. Sistemas","Vespertina", "1995131232132");
-			$m = new Maestro("Maria","Lopez","F",55,"Ing. Sistemas","Vespertina", "545454",432321);
-			echo $p->toString()."<br>";
-			echo $a->toString()."<br>";
-			echo $m->toString()."<br>";
+			$a1 = new Alumno("ElBrayan","Rodriguez","M",50,"Ing. Sistemas","Vespertina", "1995131232132");
+			$a2 = new Alumno("ElKevin","Rodriguez","M",50,"Ing. Sistemas","Vespertina", "1995131232132");
+			$m = new Maestro("LaBritani","Lopez","F",55,"Ing. Sistemas","Vespertina", "545454",432321);
+			$m1 = new Maestro("LaYolanda","Lopez","F",55,"Ing. Sistemas","Vespertina", "545454",432321);
+			$m2 = new Maestro("LaYarosla","Lopez","F",55,"Ing. Sistemas","Vespertina", "545454",432321);
+
+			$arreglo[]=$p;
+			$arreglo[]=$p1;
+			$arreglo[]=$p2;
+			$arreglo[]=$a;
+			$arreglo[]=$a1;
+			$arreglo[]=$a2;
+			$arreglo[]=$m;
+			$arreglo[]=$m1;
+			$arreglo[]=$m2;
+
+
+			mostrarAlumnos($arreglo);
+			mostrarMaestros($arreglo);
+			mostrarPersonas($arreglo);
+			
+
+			/*mostrarInformacion($p);
+			mostrarInformacion($a);
+			mostrarInformacion($m);*/
+
+			//echo $p->toString()."<br>";
+			//echo $a->toString()."<br>";
+			//echo $m->toString()."<br>";
 		?>
 	</body>
 </html>
